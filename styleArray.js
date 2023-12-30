4a879e811698a1283fbd871d6f0ed573f1781cfb
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-12-30 18:31:04 +0300

    Style image

---
+++ styleArray.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styleArray = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#25292e',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    imageContainer: {
+      flex: 1,
+      paddingTop: 58,
+    },
+    image: {
+      width: 320,
+      height: 440,
+      borderRadius: 18,
+    },
+  })
+
+  export default styleArray;
